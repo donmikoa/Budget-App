c02110018159973705642f1165c4f515b4b62c98
Michael <michael.iyaomolere@maxxpotential.com>
2020-07-10 21:51:29 -0400

    Import Transaction List

--- budget_app/src/App.js
+++ budget_app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {GlobalProvider} from './context/GlobalState'
 import Header from './components/Header'
+import TransactionList from './components/TransactionList'
 import './App.css';
 
 function App() {
--- budget_app/src/components/Transaction.js
+++ budget_app/src/components/Transaction.js
@@ -9,3 +9,64 @@ const Transaction = ({transaction}) => {
 }
 
 export default Transaction
+
+
+
+$('.charity-alphabetical').change(function () {
+	var n, t;
+	$(this).val() != ''
+		? ((n = $(this).val() == '#' ? '#\\' + this.value : '#' + this.value),
+			$('#refresh-btn').show())
+		: (n = '.letter-container:not(.letter-container-empty)'),
+		(t = $(n)),
+		$('.letter-container')
+			.not(t)
+			.hide(600),
+		t.show(600);
+});
+
+
+window.onload = function () {
+ 
+  document.getElementById("reset").onclick = function () {
+        $('.charity-type').val('select').trigger('change');
+
+        window.location.href = "https://dev.give.com/national-charity-reviews/select";
+    };
+	var selItem = sessionStorage.getItem("SelItem");
+	if (selItem == null) {
+		selItem = 'select';
+	} else {
+		$('.charity-type').val(selItem);
+	};
+	
+
+}
+
+$('.charity-type').change(function () {
+	var selVal = $(this).val();
+	sessionStorage.setItem("SelItem", selVal);
+});
+
+function accreditation(obj) {
+	if ($(obj).is(":checked")) {
+		$(".notAccredited").hide();
+	} else {
+		$(".notAccredited").show();
+    }
+}
+
+$('.directory-list').each(function () {
+	if ($.trim($(this).text()) == '' && $(this).children().length == 0) {
+		$(this).parent().hide();
+		$(this).html('<li>No charities found for chosen filter.</li>');
+	}
+});
+
+
+
+
+
+
+
+
