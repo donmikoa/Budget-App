5187226a612a614fb8df5c35484006fc9a3b3cc8
Michael <michael.iyaomolere@maxxpotential.com>
2020-07-16 08:52:00 -0400

    .

--- budget_app/src/components/IncomeExpenses.js
+++ budget_app/src/components/IncomeExpenses.js
@@ -16,7 +16,18 @@ const IncomeExpenses = () => {
 		-1
 	).toFixed(2);
 
-	return <div className="inc-exp-contaniner"></div>;
+	return (
+		<div className='inc-exp-container'>
+			<div>
+				<h4>Income</h4>
+				<p className='money plus'>{income}</p>
+			</div>
+			<div>
+				<h4>Expense</h4>
+				<p className='money minus'>{expense}</p>
+			</div>
+		</div>
+	);
 };
 
 export default IncomeExpenses;
