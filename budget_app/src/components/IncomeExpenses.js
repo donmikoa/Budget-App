5590276d54fdcf2d77d2833299bfe7190628b787
Michael <michael.iyaomolere@maxxpotential.com>
2020-07-16 08:52:29 -0400

    .

--- budget_app/src/components/IncomeExpenses.js
+++ budget_app/src/components/IncomeExpenses.js
@@ -24,7 +24,7 @@ const IncomeExpenses = () => {
 			</div>
 			<div>
 				<h4>Expense</h4>
-				<p className='money minus'>{expense}</p>
+				<p className='money minus'>{expens}</p>
 			</div>
 		</div>
 	);
