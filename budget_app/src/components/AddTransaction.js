2a1923a00ff89b335a6ece1644ab9a8385efcc6c
Michael <michael.iyaomolere@maxxpotential.com>
2020-07-19 17:38:09 -0400

    Creating the State

--- budget_app/src/components/AddTransaction.js
+++ budget_app/src/components/AddTransaction.js
@@ -4,6 +4,10 @@ import { GlobalContext } from '../context/GlobalState';
 
 
 const AddTransaction = () => {
+
+    const [text, setText] = useState(GlobalContext)
+    const [amount, setAmount] = useState(0);
+
 	return <div></div>;
 };
 
