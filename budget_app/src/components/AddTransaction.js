3b02301edac190a8712d67686e2d94f18f59b4b0
Michael <michael.iyaomolere@maxxpotential.com>
2020-07-25 09:42:09 -0400

    Creating the components

--- budget_app/src/components/AddTransaction.js
+++ budget_app/src/components/AddTransaction.js
@@ -24,6 +24,12 @@ const AddTransaction = () => {
 			<form onSubmit={onSubmit}>
 				<div className='form-control'>
 					<label htmlFor='text'>Text</label>
+					<input
+						type='text'
+						value={text}
+						onChange={(e) => setText(e.target.value)}
+						placeholder='Enter text...'
+					/>
 				</div>
 			</form>
 		</>
