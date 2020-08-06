8248d14e84eebfcb62abbd779e0fbb51897536c1
Michael <michael.iyaomolere@maxxpotential.com>
2020-08-06 13:11:20 -0400

    Created the get, post and delete routes

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -5,3 +5,20 @@
 exports.getTransactions = (req, res, next) => {
     res.send('GET transactions');
 }
+
+
+// @decs Add transactions
+// @route POST /api/v1/transactions
+// @access Public
+
+exports.addTransactions = (req, res, next) => {
+    res.send('POST transactions');
+}
+
+// @decs Delete transactions
+// @route DELETE /api/v1/transactions/:id
+// @access Public
+
+exports.deleteTransactions = (req, res, next) => {
+    res.send('Delete transactions');
+}
