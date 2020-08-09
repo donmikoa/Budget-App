b3460b5a7574bc3cd22683701346b34446958dfb
Michael <michael.iyaomolere@maxxpotential.com>
2020-08-09 18:30:34 -0400

    Created the add and get Transactions API calls.

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,27 +1,49 @@
-const Transaction = require('../models/transaction')
-
+const Transaction = require('../models/transaction');
 
 // @decs Get all transactions
 // @route GET /api/v1/transactions
 // @access Public
 
-exports.getTransactions = (req, res, next) => {
-    res.send('GET transactions');
-}
+exports.getTransactions = async (req, res, next) => {
+	try {
+		const transactions = await Transaction.find();
 
+		return res.status(200).json({
+			success: true,
+			count: transactions.length,
+			data: transactions,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			error: 'Server Error',
+		});
+	}
+};
 
 // @decs Add transactions
 // @route POST /api/v1/transactions
 // @access Public
 
-exports.addTransactions = (req, res, next) => {
-    res.send('POST transactions');
-}
+exports.addTransactions = async (req, res, next) => {
+	try {
+		const { text, amount } = req.body;
+
+		const transaction = await Transaction.create(req.body);
+
+		return res.status(201).json({
+			success: true,
+			data: transaction,
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
 
 // @decs Delete transactions
 // @route DELETE /api/v1/transactions/:id
 // @access Public
 
-exports.deleteTransactions = (req, res, next) => {
-    res.send('Delete transactions');
-}
+exports.deleteTransactions = async (req, res, next) => {
+	res.send('Delete transactions');
+};
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
 connectDB();
 
@@ -12,8 +12,15 @@ const transactions = require('./routes/transactions');
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	)
+);
